f7187c9cd2873c32e76af36dace789e53aa5f962
Fadone <fawadwakeel0550@gmail.com>
2022-11-10 15:29:59 +0500

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -4,7 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Fadone Tech
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Fadone Tech
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
